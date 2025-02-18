2c38c2b31a618f5b22bc4cdb155918fded87b757
yebinnnnn <prettybird1@naver.com>
2025-02-18 17:07:16 +0900

    [INIT]기본 BaseTimeEntity 추가

--- src/main/java/org/example/auctify/entity/Goods/GoodsEntity.java
+++ src/main/java/org/example/auctify/entity/Goods/GoodsEntity.java
@@ -2,7 +2,9 @@ package org.example.auctify.entity.Goods;
 
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
 import lombok.*;
+import org.example.auctify.entity.BaseTimeEntity;
 
 /**
  * worker : 박예빈
@@ -16,19 +18,28 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Builder
-public class GoodsEntity {
+public class GoodsEntity extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "goods_id")
     private Long goodsId;
 
-    @Column(name="goods_name", nullable = false, length = 10)
+    @Column(name="goods_name", nullable = false)
+    @Size(min=1, max=10)
     private String goodsName;
 
     @Column(name="goods_description", nullable = false, length = 2000)
+    @Size(min=20, max=2000)
     private String goodsDescription;
 
     @Column(name="first_price", nullable = false)
-    private Long goodsPrice;
+    private Long firstPrice;
+
+    @Column(name="category_type", nullable = false)
+    private String categoryType;
+
+    @Column(name="buy_now_price")
+    private Long buyNowPrice;
+
 }
