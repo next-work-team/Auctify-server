3e01349c1fb70a388f5e226577107bc36eac2edf
whdudgms <108641428+whdudgms@users.noreply.github.com>
2025-02-26 11:35:17 +0900

    [Bug] jwt-propertieserrorfix

--- src/test/java/org/example/auctify/AuctifyApplicationTests.java
+++ src/test/java/org/example/auctify/AuctifyApplicationTests.java
@@ -1,10 +1,12 @@
 package org.example.auctify;
 
+import org.example.auctify.jwt.JWTUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.jwt.secret=test-secret")
 class AuctifyApplicationTests {
+
 
     @Test
     void contextLoads() {
