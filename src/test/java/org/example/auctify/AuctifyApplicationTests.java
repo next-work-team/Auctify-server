5787c49d24be87ea3237b179de829421eb4c8658
whdudgms <108641428+whdudgms@users.noreply.github.com>
2025-02-28 18:48:26 +0900

    [Bug] test source 수정

--- src/test/java/org/example/auctify/AuctifyApplicationTests.java
+++ src/test/java/org/example/auctify/AuctifyApplicationTests.java
@@ -4,7 +4,7 @@ import org.example.auctify.jwt.JWTUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = "spring.jwt.secret=test-secret")
+@SpringBootTest
 class AuctifyApplicationTests {
 
 
