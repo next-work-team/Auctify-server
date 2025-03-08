3766e537ddc4aaa7d99bc404dfaecd61d65f9f39
whdudgms <108641428+whdudgms@users.noreply.github.com>
2025-03-08 11:47:29 +0900

    [Feat] UserControllerSwagger구현

--- src/main/java/org/example/auctify/controller/user/UserController.java
+++ src/main/java/org/example/auctify/controller/user/UserController.java
@@ -1,4 +1,59 @@
 package org.example.auctify.controller.user;
 
-public class UserController {
+import org.example.auctify.dto.user.ProfileDTO;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
+public class UserController implements UserControllerDocs{
+    @Override
+    @GetMapping("/user")
+    public ResponseEntity<?> getMyInfo(UserDetails userDetails) {
+        return null;
+    }
+
+    @Override
+    @PutMapping("/user")
+    public ResponseEntity<?> changeProfile(ProfileDTO profile, BindingResult bindingResult, UserDetails userDetails) {
+        return null;
+    }
+
+    @Override
+    @GetMapping("/TotalFeedback")
+    public ResponseEntity<?> getListTotalFeedback(UserDetails userDetails) {
+        return null;
+    }
+
+    @Override
+    @GetMapping("/MannerTemperature")
+    public ResponseEntity<?> getMannerTemperature(UserDetails userDetails) {
+        return null;
+    }
+
+    @Override
+    @GetMapping("/myBid")
+    public ResponseEntity<?> getMyBid(Long myBidId, int size, UserDetails userDetails) {
+        return null;
+    }
+
+    @Override
+    @GetMapping("/myAuctify")
+    public ResponseEntity<?> getAuctifyInfo(Long auctifyId, int size, UserDetails userDetails) {
+        return null;
+    }
+
+    @Override
+    @GetMapping("/myLiked")
+    public ResponseEntity<?> getInfiniteScrollPosts(Long likedAuctifyId, int size, UserDetails userDetails) {
+        return null;
+    }
+
+    @Override
+    @GetMapping(value = "/connect", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public ResponseEntity<?> subscribe(UserDetails userDetails, String lastEventId) {
+        return null;
+    }
 }
---
+++ src/main/java/org/example/auctify/dto/user/ProfileDTO.java
@@ -0,0 +1,43 @@
+package org.example.auctify.dto.user;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.*;
+
+import java.time.LocalDate;
+
+
+@Getter
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+public class ProfileDTO {
+
+
+    @Schema(description = "닉네임")
+    @NotNull(message = "이름은 필수입니다.")
+    @Pattern(regexp = "^[a-zA-Z가-힣0-9]{2,20}$", message = "닉네임은 2~20자의 영어, 한글, 숫자만 가능합니다.")
+    private String nickName;
+
+
+    @Schema(description = "이미지")
+    @NotNull(message = "이미지 주소")
+    private String imagedURL;
+
+
+
+    @Schema(description = "생년월일")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    @NotNull
+    private LocalDate birthDate;
+
+    //주소
+    AddressDTO addressDTO;
+
+
+
+
+}
